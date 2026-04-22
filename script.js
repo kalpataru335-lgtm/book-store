42e68c8344820e175d212137c8b358f6c3f11298
kalpataru335-lgtm <kalpataru335@gmail.com>
2026-04-22 22:46:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const books = [
-{ name:"Lilamrit (7 Vol)", price:360, weight:4, special:true, note:"7 vol sealed (except vol 1)" },
-{ name:"Hare Krishna Explosion", price:84, weight:0.5 },
-{ name:"Navadvipa Mahatmya", price:36, weight:0.3 },
-{ name:"Messenger", price:36, weight:0.3 },
-{ name:"Queen Kunti", price:24, weight:0.25 },
-{ name:"Divine Instructions", price:24, weight:0.15 },
-{ name:"Japa", price:36, weight:0.3 },
-{ name:"Lord Caitanya", price:24, weight:0.25 },
-{ name:"Path of Perfection", price:14, weight:0.2 },
-{ name:"Message of Godhead", price:6, weight:0.2 },
-{ name:"Matchless Gift", price:12, weight:0.2 },
-{ name:"Prahlada", price:12, weight:0.2 },
-{ name:"Modern Times", price:12, weight:0.2 },
-{ name:"Beyond Birth", price:8, weight:0.2 },
-{ name:"Life Comes", price:20, weight:0.3 },
-{ name:"Easy Journey", price:12, weight:0.2 },
-{ name:"Civilization", price:12, weight:0.2 },
-{ name:"Nectar", price:12, weight:0.2 },
-{ name:"HK Challenge", price:12, weight:0.2 },
-{ name:"Raja Vidya", price:12, weight:0.2 },
-{ name:"Consciousness", price:12, weight:0.2 },
-{ name:"Laws of Nature", price:20, weight:0.3 }
+{ name:"Lilamrit (7 Vol)", actual:1500, price:360, weight:4, special:true, note:"7 vol sealed (except vol 1)" },
+{ name:"Hare Krishna Explosion", actual:350, price:84, weight:0.5 },
+{ name:"Navadvipa Mahatmya", actual:150, price:36, weight:0.3 },
+{ name:"Messenger", actual:150, price:36, weight:0.3 },
+{ name:"Queen Kunti", actual:100, price:24, weight:0.25 },
+{ name:"Divine Instructions", actual:100, price:24, weight:0.15 },
+{ name:"Japa", actual:150, price:36, weight:0.3 },
+{ name:"Lord Caitanya", actual:100, price:24, weight:0.25 },
+{ name:"Path of Perfection", actual:55, price:14, weight:0.2 },
+{ name:"Message of Godhead", actual:25, price:6, weight:0.2 },
+{ name:"Matchless Gift", actual:50, price:12, weight:0.2 },
+{ name:"Prahlada", actual:50, price:12, weight:0.2 },
+{ name:"Modern Times", actual:50, price:12, weight:0.2 },
+{ name:"Beyond Birth", actual:30, price:8, weight:0.2 },
+{ name:"Life Comes", actual:80, price:20, weight:0.3 },
+{ name:"Easy Journey", actual:50, price:12, weight:0.2 },
+{ name:"Civilization", actual:50, price:12, weight:0.2 },
+{ name:"Nectar", actual:50, price:12, weight:0.2 },
+{ name:"HK Challenge", actual:50, price:12, weight:0.2 },
+{ name:"Raja Vidya", actual:50, price:12, weight:0.2 },
+{ name:"Consciousness", actual:50, price:12, weight:0.2 },
+{ name:"Laws of Nature", actual:80, price:20, weight:0.3 }
 ];
 
 let selected=[];
-
 const list=document.getElementById("book-list");
 
 books.forEach((b,i)=>{
@@ -35,7 +34,8 @@ if(b.special) div.classList.add("special");
 div.innerHTML=`
 <h4>${b.name}</h4>
 ${b.note? `<div class="note">${b.note}</div>`:""}
-<div class="new">₹${b.price}</div>
+<span class="old">₹${b.actual}</span><br>
+<span class="new">₹${b.price}</span>
 `;
 
 div.onclick=()=>toggle(i,div);
